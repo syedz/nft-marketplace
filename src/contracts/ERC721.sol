ba84e7c9d76ee760e6abc444b58293e4e31c2fe2
Zeeshan Syed <zsyed89@gmail.com>
2022-04-11 11:29:05 -0400

    Wrote approval functionality in contract

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -17,6 +17,12 @@ contract ERC721 {
     uint256 indexed tokenId
   );
 
+  event Approval(
+    address indexed owner,
+    address indexed approved,
+    uint256 indexed tokenId
+  );
+
   // Mapping in solidity creates a hash table of key pair values
 
   // Mapping from id to the owner
@@ -24,6 +30,9 @@ contract ERC721 {
 
   // Mapping from owner to number of owned tokens
   mapping(address => uint) private _OwnedTokensCount;
+
+  // Mapping from token ID to approved addresses
+  mapping(uint256 => address) private _tokenApprovals;
 
   /// @notice Count all NFTs assigned to an owner
   /// @dev NFTs assigned to the zero address are considered invalid, and this
@@ -40,7 +49,7 @@ contract ERC721 {
   ///  about them do throw.
   /// @param _tokenId The identifier for an NFT
   /// @return The address of the owner of the NFT
-  function ownerOf(uint256 _tokenId) external view returns (address) {
+  function ownerOf(uint256 _tokenId) public view returns (address) {
     address owner = _tokenOwner[_tokenId];
     require(owner != address(0), 'Owner query for non-existent token');
     return owner;
@@ -66,6 +75,63 @@ contract ERC721 {
     // keeping track of each address that is minting and adding one
     _OwnedTokensCount[to] += 1;
 
+    // address(0) is a blank address, it's the actual contract
     emit Transfer(address(0), to, tokenId);
   }
+
+  /// @notice Transfer ownership of an NFT -- THE CALLER IS RESPONSIBLE
+  ///  TO CONFIRM THAT `_to` IS CAPABLE OF RECEIVING NFTS OR ELSE
+  ///  THEY MAY BE PERMANENTLY LOST
+  /// @dev Throws unless `msg.sender` is the current owner, an authorized
+  ///  operator, or the approved address for this NFT. Throws if `_from` is
+  ///  not the current owner. Throws if `_to` is the zero address. Throws if
+  ///  `_tokenId` is not a valid NFT.
+  /// @param _from The current owner of the NFT
+  /// @param _to The new owner
+  /// @param _tokenId The NFT to transfer
+  function _transferFrom(address _from, address _to, uint256 _tokenId) internal {
+    /**
+      1. Add the token Id to the address receiving the token
+      2. Update the balance of the address _from token
+      3. Update the balance of the address _to
+      4. Add the safe functionality:
+        a. Require that the address receiving a token is not a zero address
+        b. Require the address transferring the token actually owns the token
+     */
+
+    require(_to != address(0), 'Error - ERC721 Transfer to the zero address');
+    require(ownerOf(_tokenId) == _from, 'Trying to transfer a token the address does not own!');
+
+    _OwnedTokensCount[_from] -= 1;
+    _OwnedTokensCount[_to] += 1;
+
+    _tokenOwner[_tokenId] = _to;
+
+    emit Transfer(_from, _to, _tokenId);
+  }
+
+  function transferFrom(address _from, address _to, uint256 _tokenId) public {
+    // require(isApprovedOrOwner(msg.sender, _tokenId));
+    _transferFrom(_from, _to, _tokenId);
+  }
+
+  function approve(address _to, uint256 tokenId) public {
+    // 1. Require that the person approve is the owner
+    // 2. We are approving an address to a token (tokenId)
+    // 3. Require that we can't approve sending tokens of the owner to the owner (current caller - ourselves)
+    // 4. Update the map of the approval addresses
+
+    address owner = ownerOf(tokenId);
+    require(_to != owner, 'Error - approval to current owner');
+    require(msg.sender == owner, 'Current caller is not hte owner of the token');
+    _tokenApprovals[tokenId] = _to;
+
+    emit Approval(owner, _to, tokenId);
+  }
+
+  // function isApprovedOrOwner(address spender, uint256 tokenId) internal view returns(bool) {
+  //   require(_exists(tokenId), 'Token does not exist');
+  //   address owner = ownerOf(tokenId);
+  //   return(spender == owner || getApproved(tokenId) == spender);
+  // }
 }
