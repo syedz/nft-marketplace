e03fe59d47e34827e63f37dd36ad07ebaf35fc4d
Zeeshan Syed <zsyed89@gmail.com>
2022-04-16 14:36:44 -0400

    Created more tests

--- test/KryptoBird.test.js
+++ test/KryptoBird.test.js
@@ -9,13 +9,15 @@ require('chai')
 
 contract('KryptoBird', (accounts) => {
   let contract;
-  
+  before(async () => {
+    contract = await KryptoBird.deployed();
+  });
+
   // testing container - describe
 
   describe('deployment', async () => {
     // test samples with writing it
     it('deploys successfully', async () => {
-      contract = await KryptoBird.deployed();
       const address = contract.address;
       assert.notEqual(address, '');
       assert.notEqual(address, null);
@@ -31,6 +33,46 @@ contract('KryptoBird', (accounts) => {
     it('has a symbol', async () => {
       const name = await contract.symbol()
       assert.equal(name, 'KBIRDZ');
+    });
+  });
+
+  describe('minting', async () => {
+    it('creates a new token', async () => {
+      const result = await contract.mint('https...1');
+      const totalSupply = await contract.totalSupply();
+
+      // Success 
+      assert.equal(totalSupply, 1);
+      const event = result.logs[0].args;
+      assert.equal(event._from, '0x0000000000000000000000000000000000000000', 'from the contract');
+      assert.equal(event._to, accounts[0], 'to is msg.sender');
+
+      // Failure - Can't mint same NFT
+      await contract.mint('https...1').should.be.rejected;
+    });
+  });
+
+  describe('indexing', async () => {
+    it('lists KryptoBirdz', async () => {
+      // Mint 3 new tokens
+      await contract.mint('https...2');
+      await contract.mint('https...3');
+      await contract.mint('https...4');
+
+      const totalSupply = await contract.totalSupply();
+
+      // Loop through list and grab KBirdz from
+      let result = [];
+      let KryptoBird;
+  
+      for (i = 1; i <= totalSupply; i++) {
+        KryptoBird = await contract.kryptoBirdz(i - 1);
+        result.push(KryptoBird);
+      }
+
+      // Assert that our new array result will equal our expected result
+      let expected = ['https...1', 'https...2', 'https...3', 'https...4'];
+      assert.equal(result.join(','), expected.join(','))
     });
   });
 });
